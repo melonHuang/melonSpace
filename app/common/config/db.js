272b1057ad989f07cdf4ae2416000a69c933276b
melonHuang <melonh0327@gmail.com>
2017-02-21 16:14:01 +0800

    first commit

---
+++ app/common/config/db.js
@@ -0,0 +1,18 @@
+
+      "use strict";
+      exports.__esModule = true;
+      exports.default = {
+    "type": "mysql",
+    "adapter": {
+        "mysql": {
+            "host": "23.105.201.130",
+            "port": "3306",
+            "database": "melonspace",
+            "user": "huangwei",
+            "password": "477819hww",
+            "prefix": "fk_",
+            "type": "mysql"
+        }
+    }
+}
+    
